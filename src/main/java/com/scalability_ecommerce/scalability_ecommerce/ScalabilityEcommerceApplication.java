77fe40218e59ab970f23e441a38f9299e66da93b
Abhishek Parashar <aparashar103@gmail.com>
2025-08-18 23:23:28 +0530

    first commit

---
+++ src/main/java/com/scalability_ecommerce/scalability_ecommerce/ScalabilityEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.scalability_ecommerce.scalability_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalabilityEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalabilityEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scalability_ecommerce/scalability_ecommerce/ScalabilityEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scalability_ecommerce.scalability_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalabilityEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
