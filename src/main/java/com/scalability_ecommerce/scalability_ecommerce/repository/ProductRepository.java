5ac2010139e368195ec9181493735f9baf7221c2
Abhishek Parashar <aparashar103@gmail.com>
2025-08-20 23:39:54 +0530

    wip

--- src/main/java/com/scalability_ecommerce/scalability_ecommerce/repository/ProductRepository.java
+++ src/main/java/com/scalability_ecommerce/scalability_ecommerce/repository/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.scalability_ecommerce.scalability_ecommerce.repository;
 
-public class ProductRepository {
+import com.scalability_ecommerce.scalability_ecommerce.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    // Optimized query with caching hint
+
 }
